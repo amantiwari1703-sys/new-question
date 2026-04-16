ec1cda01c4bcffe56e7cc51c3e4877731306e456
Aman Tiwari <amantiwari@gmail.com>
2026-04-16 22:56:39 +0530

    wear

---
+++ daman.java
@@ -0,0 +1,26 @@
+public class daman {
+    public static void main(String[] args){
+        dhruv  d= new dhruv();
+        dhruv.wear g=d.new wear();
+        System.out.println(d.o+g.p);
+
+
+
+
+
+
+
+    }
+
+}
+
+ class dhruv {
+    int o = 2;
+
+
+public class wear{
+    int p=3;
+
+}
+
+}
---
+++ python.java
@@ -0,0 +1,21 @@
+public class python {
+    public static void main(String[] args){
+        naman n=new naman();
+        naman.ankit s=n.new ankit();
+        System.out.println(n.a+s.b);
+
+
+
+    }
+}
+
+class naman{
+    int a=27;
+
+class ankit{
+    int b=36;
+
+
+
+}
+}
