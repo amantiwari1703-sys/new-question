260d33affca769b8dc9593f0b0f9816b25021ab0
Aman Tiwari <amantiwari@gmail.com>
2026-04-17 08:26:44 +0530

    abst

---
+++ abst.java
@@ -0,0 +1,17 @@
+public class abst {
+    public static void main(String[] args){
+        yuvi  y=new yuvi();
+         y.grow();
+
+
+
+    }
+
+}
+class yuvi extends abst{
+    public void grow() {
+        System.out.println("height");
+
+    }
+
+}
